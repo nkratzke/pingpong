f7399ab0d93bc1f973918146d67f0035f2456f8f
Nane Kratzke <nane.kratzke@googlemail.com>
2014-10-10 14:07:36 +0200

    Updated install script to work without interaction

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 
 # Script to install dart sdk on a linux ubuntu system
 apt-get update
-apt-add-repository ppa:hachre/dart
-apt-get install dartsdk
+apt-add-repository ppa:hachre/dart -y
+apt-get install dartsdk -y
 
 # Install
 pub get
+
+echo "now you can run 'dart bin/ping.dart' to start the ping server
+echo "or you can run 'dart bin/pong.dart' to start the pong server
