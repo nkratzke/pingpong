555562561748d930db2a7b5d3b0eb708072fcbec
Nane Kratzke <nkratzke@users.noreply.github.com>
2014-10-15 16:08:54 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ do
   do
 
     # run each benchmark 10 times
-    for round in 1..10
+    for round in 1 2 3 4 5 6 7 8 9 10
     do
       siege -c $USER -b -t 10s --log=siege-$USER-$LEN.log $URL/$LEN
     done
