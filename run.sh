253e91c2a613d646d555d1ccdd01a0459407b099
Nane Kratzke <nkratzke@users.noreply.github.com>
2014-10-15 16:06:24 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,17 +5,17 @@
 URL=http://my.host.com/ping
 
 # Starts the experiment with following amount of users.
-for USER in 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000
+for USER in 100 200 300 400 500 600 700 800 900 1000
 do
 
   # each requesting messages of length (in bytes)
-  for LEN in 128, 256, 512, 768, 1024, 2048, 4096, 8192
+  for LEN in 128 256 512 1024 2048 4096 8192
   do
 
     # run each benchmark 10 times
     for round in 1..10
     do
-      siege -c $USER -b -t 10s -l siege-$USER-$LEN.log $URL/$LEN
+      siege -c $USER -b -t 10s --log=siege-$USER-$LEN.log $URL/$LEN
     done
 
   done
