7facf1dc6a71be601f897bbcb8bfdd68b9b39eec
Nane Kratzke <nane.kratzke@googlemail.com>
2014-10-16 10:56:53 +0200

    Provided some inline docs for the analyze script

--- bin/analyze.dart
+++ bin/analyze.dart
@@ -1,15 +1,15 @@
 import 'dart:io';
 
 void main(args) {
-  
-  print("Program to extract some data from apache bench log files");
-  
+    
   final fname = args[0];
   final file = new File(fname);
   file.readAsString().then((log) {
     final data = log.split("This is ApacheBench,").map((benchrun) {
-      if (benchrun.isEmpty) return "";
+      // Each benchmark log entry starts with "This is ApacheBench"
+      if (benchrun.isEmpty) return ""; // Skip to first empty entry
       
+      // Reduce benchmark log to relevant data
       final lines = benchrun.split("\n").where((line) =>
         line.startsWith("Document Path:") ||
         line.startsWith("Document Length:") ||
@@ -22,8 +22,9 @@ void main(args) {
         line.startsWith("Transfer rate:")
       );
       
-      if (lines.isEmpty) return "";
+      if (lines.isEmpty) return ""; // Skip non successfull benchmark runs (they are empty)
             
+      // Read relevant data
       final document = lines.where((String line) => line.startsWith("Document Path:")).first;
       final length = lines.where((String line) => line.startsWith("Document Length:")).first;
       final testtime = lines.where((String line) => line.startsWith("Time taken for tests:")).first;
@@ -34,14 +35,17 @@ void main(args) {
       final rps = lines.where((String line) => line.startsWith("Requests per second:")).first;
       final trans = lines.where((String line) => line.startsWith("Transfer rate:")).first;
       
+      // Define some regular expression matchers to read data
       final pingMatcher = new RegExp(r'/ping/\d+');
       final intMatcher = new RegExp(r'\d+');
       final floatMatcher = new RegExp(r'[-+]?(\d*[.])?\d+');
       
+      // Some helpers to apply the above matchers
       final getPing = (s) => pingMatcher.firstMatch(s).group(0);
       final getInt = (s) => intMatcher.firstMatch(s).group(0);
       final getFloat = (s) => floatMatcher.firstMatch(s).group(0);
       
+      // Return a csv line by applying the matchers
       return
         "${getPing(document)}, " 
         "${getInt(length)}, "
@@ -63,7 +67,7 @@ void main(args) {
      "Failed requests", 
      "Concurrency Level", 
      "Total transferred",
-     "Requests per second",
+     "Requests per second (mean)",
      "Transfer rate"
     ];
     
