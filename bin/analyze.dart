1aa639942d0b1994c3032fc6fa032b93033a6afd
Nane Kratzke <nkratzke@users.noreply.github.com>
2014-10-16 13:02:21 +0200

    Fixed testtime extraction

--- bin/analyze.dart
+++ bin/analyze.dart
@@ -49,7 +49,7 @@ void main(args) {
       return
         "${getPing(document)}, " 
         "${getInt(length)}, "
-        "${getFloat(length)}, "
+        "${getFloat(testtime)}, "
         "${getInt(completed)}, "
         "${getInt(failed)}, "
         "${getInt(concurrency)}, "
