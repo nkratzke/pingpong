78bc44ac86ca1537649d3df0599e3455627b79a1
Nane Kratzke <nane.kratzke@googlemail.com>
2015-01-23 13:49:49 +0100

    Fixed 10000000->1E 06 Bug (due to seq)

--- run.sh
+++ run.sh
@@ -8,35 +8,35 @@ URL=http://my.host.com/ping
 USER=50
 
 # Repeat each benchmarking run 20 times
-for round in $(seq 1 20)
+for ((round=1; round<=20; round++))
 do
 
   # Small message sizes (10, 20, ... 100) bytes
-  for LEN in $(seq 10 10 100)
+  for ((LEN=10; LEN<100; LEN=LEN+10))
   do
     ab -c $USER -n 1000 $URL/$LEN >> apachebench.log
   done
 
   # Sub kilobyte message sizes (100, 200, ..., 1000) bytes
-  for LEN in $(seq 100 100 1000)
+  for ((LEN=100; LEN<1000; LEN=LEN+100))
   do
     ab -c $USER -n 1000 $URL/$LEN >> apachebench.log
   done
 
   # kilobyte message sizes (1000, 2000, ..., 10000) bytes
-  for LEN in $(seq 1000 1000 10000)
+  for ((LEN=1000; LEN<10000; LEN=LEN+1000))
   do
     ab -c $USER -n 1000 $URL/$LEN >> apachebench.log
   done
 
   # 10 kByte message sizes (10kB, 20kB, ..., 300kB)
-  for LEN in $(seq 10000 10000 300000)
+  for ((LEN=10000; LEN<300000; LEN=LEN+10000))
   do
     ab -c $USER -n 1000 $URL/$LEN >> apachebench.log
   done
 
-  # 100kByte messages sizes (300kB, 400kB, ..., 500kB)
-  for LEN in $(seq 300000 100000 500000)
+  # 100kByte messages sizes (300kB, 400kB, ..., 1000kB)
+  for ((LEN=300000; LEN<=1000000; LEN=LEN+100000))
   do
     ab -c $USER -n 1000 $URL/$LEN >> apachebench.log
   done
