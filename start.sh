7606613ea849f5b144c80cd921110d62809d2ce9
Nane Kratzke <nane.kratzke@googlemail.com>
2015-07-07 14:48:56 +0200

    Added simplifying start script

---
+++ start.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Script to start the ping and pong services
+#
+# 'start.sh bare pong' starts the pong server in bare mode
+# 'start.sh bare ping http://<pongip>:<pongport>' starts the ping server in bare mode
+# 'start.sh docker pong' starts the pong server as docker container
+# 'start.sh docker ping http://<pongip>:<pongport>' starts the ping server as docker container
+# 'start.sh weave pong' starts the pong server as docker container and connects it to a weave network
+# 'start.sh weave ping http://<weavepongip>:<pongport> ponghostip' starts the ping server as docker container and connects it to a weave network
+
+mode=$1
+service=$2
+pongip=$3
+ponghostip=$4
+
+function bare {
+
+	case "$1"
+	
+	ping) sudo dart bin/ping.dart --port=8080 -url="http://$2:8080"
+		  ;;
+		  
+	pong) sudo dart bin/pong.dart --port=8080
+	      ;;
+	
+	esac
+}
+
+function docker {
+
+	case "$1"
+	
+	ping) sudo docker build -t pingpong .
+	      sudo docker run -d -p 8080:8080 pingpong --asPing --port=8080 --url="http://$2:8080"
+		  ;;
+	pong) sudo docker build -t pingpong .
+	      sudo docker run -d -p 8080:8080 pingpong --asPong --port=8080
+	      ;;
+	esac
+}
+
+function weave {
+	case "$1"
+	
+	ping)
+	
+	pong)
+	
+	esac
+}
+
+case "$mode" in
+bare)   bare(service, pongip)
+        ;;
+docker) docker(service, pongip)
+        ;;
+weave)  weave(service, pongsdnip, ponghostip)
+        ;;
+esac
+
+
