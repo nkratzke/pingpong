1cf2e9519c9f38309299fc79923832e82542d423
Nane Kratzke <nkratzke@users.noreply.github.com>
2015-07-26 10:50:25 +0200

    Added some cool down phases
    to avoid provider network overload

--- run.sh
+++ run.sh
@@ -15,24 +15,28 @@ do
   for ((LEN=10; LEN<100; LEN=LEN+10))
   do
     ab -c $USER -n 1000 $URL/$LEN >> apachebench.log
+    sleep 1 # cool down
   done
 
   # Sub kilobyte message sizes (100, 200, ..., 1000) bytes
   for ((LEN=100; LEN<1000; LEN=LEN+100))
   do
     ab -c $USER -n 1000 $URL/$LEN >> apachebench.log
+    sleep 1 # cool down
   done
 
   # kilobyte message sizes (1000, 2000, ..., 10000) bytes
   for ((LEN=1000; LEN<10000; LEN=LEN+1000))
   do
     ab -c $USER -n 1000 $URL/$LEN >> apachebench.log
+    sleep 5 # cool down
   done
 
   # 10 kByte message sizes (10kB, 20kB, ..., 250kB)
-  for ((LEN=10000; LEN<250000; LEN=LEN+10000))
+  for ((LEN=10000; LEN<260000; LEN=LEN+10000))
   do
     ab -c $USER -n 1000 $URL/$LEN >> apachebench.log
+    sleep 5 # cool down
   done
 
   # Most of the times not gives additional insights
@@ -40,6 +44,7 @@ do
   # for ((LEN=300000; LEN<=1000000; LEN=LEN+100000))
   # do
   #  ab -c $USER -n 1000 $URL/$LEN >> apachebench.log
+  #  sleep 10 # cool down
   # done
 
 done
