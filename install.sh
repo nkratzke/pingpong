1d01130267cd6dda474a8e556ddf23d5f7243df6
Nane Kratzke <nane.kratzke@googlemail.com>
2015-08-22 15:43:20 +0200

    Updated install for ruby

--- install.sh
+++ install.sh
@@ -12,6 +12,8 @@ sudo apt-get install apache2-utils conntrack curl -y
 # Install ruby and ppbench
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
+source /usr/local/rvm/scripts/rvm
+sudo chmod o+w /usr/local/rvm/gems/* --recursive
 bundle install --gemfile=./ppbench/Gemfile
 
 # Install docker
