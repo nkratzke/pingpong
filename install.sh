ebf7418a5cd678802f1ebcc6ab5e2f0f3871aecf
Nane Kratzke <nane.kratzke@googlemail.com>
2015-09-05 19:00:44 +0200

    Switching to official dart channel

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-# Install dart from the stable channel
+# Install dart from the stable and official channel
 sudo apt-get update
-sudo apt-add-repository ppa:hachre/dart
+sudo apt-get install apt-transport-https
+sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
 sudo apt-get update
-sudo apt-get install dartsdk -y
+sudo apt-get install dart -y
 
 # Install apache benchmark, conntrack, curl
 sudo apt-get install apache2-utils conntrack curl -y
